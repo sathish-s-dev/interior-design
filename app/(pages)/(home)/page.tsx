fa9a1d6593c258e62e3dbd6839eea911027d29ec
sathish s <s.sathishmechddesign@gmail.com>
2024-12-12 12:25:39 +0530

    fix: unused link import removed

--- app/(pages)/(home)/page.tsx
+++ app/(pages)/(home)/page.tsx
@@ -4,10 +4,7 @@ import Offering from "./(ui)/offering";
 import ScrollVideo from "./(ui)/scroll-video";
 import Announcement from "./(ui)/announcement";
 import Services from "./(ui)/services";
-import Link from "next/link";
-import { ArrowRight } from "lucide-react";
-import { webLinks } from "@/app/constants";
-// import Projects from "./(ui)/projects";
+
 
 export const metadata: Metadata = {
   title: "Interior Design",
